0fa1b1dc5ff02f30d6a157f8ff90dd204fa9da04
Colby Birkhead <birkheadc@gmail.com>
2023-04-07 11:01:33 +0900

    add packages for jest testing

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,5 +1,10 @@
+import * as React from 'react';
+import '@testing-library/jest-dom';
+import { render, fireEvent, screen } from '@testing-library/react';
+import App from './App';
+
 describe('app', () => {
-  it('works', () => {
+  it('renders', () => {
     expect(true).toBe(true);
   })
 });
