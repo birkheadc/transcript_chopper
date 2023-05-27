37a80bf848a84d3932f50cb27c3449825797c1cc
Colby Birkhead <birkheadc@gmail.com>
2023-05-27 18:09:20 +0900

    create first integration test

--- src/components/chopper/chopper/Chopper.test.js
+++ src/components/chopper/chopper/Chopper.test.js
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import Chopper from './Chopper'
+import { render } from '@testing-library/react';
 
 describe('Chopper', () => {
-  it('', () => {
-    
+  it('renders without crashing', () => {
+    renderChopper();
   });
 });
+
+// Helpers
+
+function renderChopper() {
+  render(<Chopper />);
+}
